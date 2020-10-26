849a76a96fd568e95359d9be1e8ddf47a7ded990
agorararmard <aagouhar@efabless.com>
2020-10-26 21:42:54 +0200

    base checks as implemented by @matdrapeau

---
+++ base_checks/check_license.py
@@ -0,0 +1,36 @@
+import os
+import hashlib
+from urllib.request import urlopen
+
+
+_license_filename = 'LICENSE.txt'
+_lib_license_filename = 'LICENSE.txt'
+_apache_license_url = 'http://www.apache.org/licenses/LICENSE-2.0.txt'
+
+def check_main_license(path):
+    data = urlopen(_apache_license_url).read()
+    remote_hash = hashlib.md5(data)
+    try:
+        local_hash = hashlib.md5(open(os.path.join(path, _license_filename), 'rb').read())
+        return remote_hash.hexdigest() == local_hash.hexdigest()
+    except OSError:
+        return False
+    
+def check_lib_license(path):
+    libs = []
+    if os.path.exists(path):
+        for lib_path in next(os.walk(path))[1]:
+            try:
+                size = os.path.getsize(os.path.join(lib_path, _lib_license_filename))
+                libs.append((lib_path, bool(size)))
+            except OSError:
+                libs.append((lib_path, False))
+    return libs
+    
+if __name__ == "__main__":
+    if check_main_license('.'):
+        print("License there!")
+    else:
+        print("License not there or empty!")
+
+    print(check_lib_license('.'))
---
+++ base_checks/check_yaml.py
@@ -0,0 +1,29 @@
+import os
+import yaml
+from typing import NamedTuple
+
+_yaml_filename = 'info.yaml'
+
+class YamlStructure(NamedTuple):
+    name: str
+    description: str
+    git_url: str
+    version: int = 1
+
+
+def check_yaml(path):
+    try:
+        content = yaml.load(open(os.path.join(path, _yaml_filename)).read())
+        obj = YamlStructure(**content)
+        return True
+    except TypeError as e:
+        return False
+    except FileNotFoundError as e:
+        return False
+    
+    
+if __name__ == "__main__":
+    if check_yaml('.'):
+        print("YAML file valid!")
+    else:
+        print("YAML file not valid!")
