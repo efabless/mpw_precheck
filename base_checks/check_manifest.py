f87af1d983689b47b327ef45f2a264c1c0810949
agorararmard <aagouhar@efabless.com>
2020-12-08 21:32:33 +0200

    fix non-default arg following default arg

--- base_checks/check_manifest.py
+++ base_checks/check_manifest.py
@@ -25,17 +25,17 @@ mag_manifest_git_url="https://raw.githubusercontent.com/efabless/caravel/develop
 default_logger_path = '/usr/local/bin/full_log.log'
 default_target_path = '/usr/local/bin/caravel/'
 
-def check_manifest(target_path, output_file,lc=logging_controller(default_logger_path,default_target_path),call_path='/usr/local/bin/base_checks', git_url):
+def check_manifest(target_path, output_file, git_url, lc=logging_controller(default_logger_path,default_target_path),call_path='/usr/local/bin/base_checks'):
     path=Path(target_path)
     if not os.path.exists(path):
         return False,str(target_path)+" not found"
     call_path = os.path.abspath(call_path)
     
-    run_manifest_check_cmd = "sh {call_path}/shasum_manifest.sh {target_path} {manifest_file_name} {manifest_git_url} {output_file}".format(
+    run_manifest_check_cmd = "sh {call_path}/shasum_manifest.sh {target_path} {manifest_file_name} {git_url} {output_file}".format(
         call_path=call_path,
         target_path=target_path,
         manifest_file_name=manifest_file_name,
-        manifest_git_url=manifest_git_url,
+        git_url=git_url,
         output_file=output_file
     )
 
@@ -73,10 +73,10 @@ def check_manifest(target_path, output_file,lc=logging_controller(default_logger
 
 
 def check_manifests(target_path, output_file,lc=logging_controller(default_logger_path,default_target_path),call_path='/usr/local/bin/base_checks'):
-    check, reason, fail_lines = check_manifest(target_path+'/verilog/rtl',str(output_file)+'.rtl.log',lc,call_path,rtl_manifest_git_url)
+    check, reason, fail_lines = check_manifest(target_path+'/verilog/rtl', str(output_file)+'.rtl.log', rtl_manifest_git_url, lc, call_path)
     if check:
-        check, reason, fail_lines = check_manifest(target_path+'/maglef',str(output_file)+'.maglef.log',lc,call_path,maglef_manifest_git_url)
+        check, reason, fail_lines = check_manifest(target_path+'/maglef', str(output_file)+'.maglef.log', maglef_manifest_git_url, lc, call_path)
         if check:
-            check, reason, fail_lines = check_manifest(target_path+'/mag',str(output_file)+'.mag.log',lc,call_path,mag_manifest_git_url)    
+            check, reason, fail_lines = check_manifest(target_path+'/mag', str(output_file)+'.mag.log', mag_manifest_git_url, lc, call_path)    
     return check, reason, fail_lines
 
