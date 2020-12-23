05aec77b25afaaabd8010da364cf60016025176d
Jeff DiCorpo <jeffdi@efabless.com>
2020-12-23 20:41:29 +0000

    updated scripts

--- run_precheck.sh
+++ run_precheck.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 open_mpw_prechecker.py -wfc --pdk_root $PDK_ROOT --target_path $TARGET_PATH
+python3 open_mpw_prechecker.py -wfc -dc --pdk_root $PDK_ROOT --target_path $TARGET_PATH
