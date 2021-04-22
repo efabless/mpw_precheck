484eff230a66a83a16a431cd5e6b52496def73ed
Jeff DiCorpo <42048757+jeffdi@users.noreply.github.com>
2021-04-21 18:31:54 -0700

    Update run_precheck.sh
    added new -c parameter for caravel root

--- run_precheck.sh
+++ run_precheck.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 open_mpw_prechecker.py -wfc -dc --pdk_root $PDK_ROOT --target_path $TARGET_PATH
+python3 open_mpw_prechecker.py -wfc -dc --pdk_root $PDK_ROOT --target_path $TARGET_PATH -c $TARGET_PATH/caravel/
