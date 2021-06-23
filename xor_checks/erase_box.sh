98dcb85009e6b8150b41c47931a734aa4842b2df
jeffdi <jeffdi@efabless.com>
2021-06-23 12:49:58 -0700

    EFIC-78 fix erase_box.sh

--- xor_checks/erase_box.sh
+++ xor_checks/erase_box.sh
@@ -13,7 +13,7 @@ echo "$1 $2 $3 $4 $5 $6 $7"
 
 export PDK=sky130A
 
-export MAGIC_MAGICRC=$PDK_ROOT/$PDK/libs.tech/magic/$PDK.magicrc
+export MAGIC_MAGICRC
 
 MAGTYPE=mag magic -rcfile $MAGIC_MAGICRC -dnull -noconsole  <<EOF
 drc off
@@ -23,10 +23,15 @@ cif istyle sky130(vendor)
 gds read $1
 load $7
 select top cell
-flatten -nolabels xor_target
-load xor_target
-box $2um $3um $4um $5um
-erase
+box 0 0 20um 3520um
+flatten -nolabels -dobox $7
+box 2900um 0 2920um 3520um
+flatten -nolabels -dobox $7
+box 20 0 2900um 20um
+flatten -nolabels -dobox $7
+box 20 3500um 2900um 3520um
+flatten -nolabels -dobox $7
+load $7
 gds write $6
 quit -noprompt
 EOF
