a1c6918d6effe07e02eff6f78c1f61cf33f3ccef
jeffdi <jeffdi@efabless.com>
2021-06-23 14:51:12 -0700

    EFIC-78 fix erase_box.sh

--- xor_checks/erase_box.sh
+++ xor_checks/erase_box.sh
@@ -19,15 +19,15 @@ cif istyle sky130(vendor)
 gds read $1
 load $3
 select top cell
-box 0 0 20um 3520um
-flatten -nolabels -dobox $3
-box 2900um 0 2920um 3520um
-flatten -nolabels -dobox $3
-box 20 0 2900um 20um
-flatten -nolabels -dobox $3
-box 20 3500um 2900um 3520um
-flatten -nolabels -dobox $3
-load $3
+box -20um 0 0 3520um
+flatten -nolabels -dobox xor_target
+box 2920um 0 2940um 3520um
+flatten -nolabels -dobox xor_target
+box -20um -20um 2940um 0
+flatten -nolabels -dobox xor_target
+box -20um 3520um 2940um 3540um
+flatten -nolabels -dobox xor_target
+load xor_target
 gds write $2
 quit -noprompt
 EOF
