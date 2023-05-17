6b9b97ba948575911e6938c28e2107f2121ad334
marwaneltoukhy <marwaneltoukhy@aucegypt.edu>
2023-05-17 19:26:22 +0300

    changed default to False for --disable-lvs

--- mpw_precheck.py
+++ mpw_precheck.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--pdk_path', required=True, help="PDK_PATH, points to the installation path of the pdk (variant specific)")
     parser.add_argument('-o', '--output_directory', required=False, help="OUTPUT_DIRECTORY, default=<input_directory>/precheck_results/DD_MMM_YYYY___HH_MM_SS.")
     parser.add_argument('--private', action='store_true', help=f"If provided, precheck skips {open_source_checks.keys() - private_checks.keys()}  checks that qualify the project to be Open Source")
-    parser.add_argument('--disable-lvs', action='store_true', help=f"If provided, precheck skips LVS - CVC - Soft connections checks that qualify the project to be Open Source")
+    parser.add_argument('--disable-lvs', action='store_true', default=False, help="If provided, precheck skips LVS - CVC - Soft connections checks that qualify the project to be Open Source")
     parser.add_argument('checks', metavar='check', type=str, nargs='*', choices=list(open_source_checks.keys()).append([]), help=f"Checks to be run by the precheck: {' '.join(open_source_checks.keys())}")
     args = parser.parse_args()
 
