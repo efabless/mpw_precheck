6396284c51b2c91ded63583a378bb6403345eb95
marwaneltoukhy <marwaneltoukhy@aucegypt.edu>
2023-11-02 13:46:45 +0200

    fixed the XOR check to avoid erasing met5 and met4 from outside the PR boundary

--- checks/xor_check/erase_box_openframe.tcl
+++ checks/xor_check/erase_box_openframe.tcl
@@ -11,24 +11,18 @@ select top cell
 #
 # flatten perimeter outside of PR boundary including power rings
 #
-box 0 0 3166.63um -2um
+box 0 0 3166.63um -210.685um
 flatten -nolabels -dobox xor_target
-box 3166.63um 0 3168.63um 4766.63um
+box 3166.63um 0 3377.315um 4766.63um
 flatten -nolabels -dobox xor_target
-box 3166.63um 4766.63um 0 4768.63um
+box 3166.63um 4766.63um 0 4977.315um
 flatten -nolabels -dobox xor_target
-box 0 4766.63um -2um 0
+box 0 4766.63um -210.685um 0
 flatten -nolabels -dobox xor_target
-#
-# load new cell and erase power straps from user_project_area
-load xor_target
-select top cell
-erase metal5
-erase metal4
-erase metal5
-erase metal4
+
 #
 # write gds
 #
+load xor_target
 gds write $out_file
 quit -noprompt
